c9417a8468563a535c6ef9a0b520e50af75949c1
Dayane <acdayane@gmail.com>
2022-11-12 22:05:07 -0300

    feat: list participants

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
-import dayjs from 'dayjs';
 
 const app = express();
 dotenv.config();
@@ -11,8 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 const nameSchema = joi.object({
-  name: joi.string().required().min(2).max(30),
+  name: joi.string().required().min(2).max(30)
 });
+
+let time = new Date();
+let currentTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+let lastStatus = Date.now();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
@@ -28,48 +31,44 @@ let collectionParticipants = db.collection('participants');
 let collectionMessages = db.collection('messages');
 
 app.post('/participants', async (req, res) => {
-  const body = req.body;
+  const {name} = req.body;
 
-  const newUser = {
-    name: body,
-    lastStatus: Date.now()
-  }
-
-  const firstMessage = {
-    from: body.name,
-    to: 'Todos',
-    text: 'entra na sala...',
-    type: 'status',
-    time: 'HH:MM:SS'
-  }
-
-  const validation = nameSchema.validate(body, {abortEarly: false});
+  const validation = nameSchema.validate({name}, {abortEarly: false});
   
   if (validation.error) {
     const errors = validation.error.details.map((d) => d.message);
     res.send(errors);
     return;
   }
-
-  // let date = [dayjs().locale("pt").format("dddd, D/MM/YYYY")]
-  // let horas = now.getHours()
   
   try {
-    const responseParticipant = await collectionParticipants.insertOne({ newUser });
+    const responseParticipant = await collectionParticipants.insertOne({ name, lastStatus: lastStatus });
     res.sendStatus(201);
     console.log(responseParticipant)
   } catch (err) {
-    console.log(err);
+    res.status(500).send(err);
   }
 
   // try {
-  //   const responseMessage = await collectionMessages.insertOne({ firstMessage });
+  //   const responseMessage = await collectionMessages.insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: currentTime});
   //   res.sendStatus(201);
   //   console.log(responseMessage)
   // } catch (err) {
   //   console.log(err);
   // }
 })
+
+app.get('/participants', async (req, res) => {
+  try {
+    const participants = await collectionParticipants.find().toArray();
+    console.log(participants);
+    res.sendStatus(200);   
+  } catch (err) {
+    res.status(500).send(err);
+  }
+  
+});
+
 
 
 
